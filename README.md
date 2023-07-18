6d230d683b3b686dcf3808537a678275f0fae0cb
Ahmed <imaneben8@gmail.com>
2023-07-18 22:41:50 +0100

    inital commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React Props and State
+npm # React Props and State
 
 Welcome to the wonderful world of React!
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import './App.css';
-import Navbar from './components/navbar';
-import Header from './components/header';
-import Footer from './components/footer';
+import "./App.css";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 function App(): JSX.Element {
+  // JavaScript/TypeScript code can be inserted here!
 
-	// JavaScript/TypeScript code can be inserted here!
+  return (
+    <>
+      <Navbar />
+      <Header />
 
-	return (
-		<>
-			<Navbar />
-			<Header />
+      <main>
+        <div className="cards__wrapper">{/* JSX code can go here */}</div>
+      </main>
 
-			<main>
-				<div className='cards__wrapper'>{/* JSX code can go here */}</div>
-			</main>
-
-			<Footer />
-		</>
-	);
+      <Footer />
+    </>
+  );
 }
 
 export default App;
